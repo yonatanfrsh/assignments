e6a5de6605797ffacde78dbc31e6348b52332b0c
yonatanfrsh <105312807+yonatanfrsh@users.noreply.github.com>
2022-05-11 09:08:32 +0300

    second

--- src/app/services/assignment-type.service.ts
+++ src/app/services/assignment-type.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AssignmentType } from '../models/assignment-type';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,14 @@ export class AssignmentTypeService {
   constructor(private http: HttpClient) { }
 
   public getAssignmentsTypes(): Observable<AssignmentType[]> {
-    return this.http.get<AssignmentType[]>("http://localhost:5286/api/AssignmentsTypes/GetAssignmentsTypesAsync");
+    return this.http.get<AssignmentType[]>(environment.baseUrlApi + "api/AssignmentsTypes/GetAssignmentsTypesAsync");
   }  
 
   public newAssignmentsTypes(assignmentType: AssignmentType): Observable<AssignmentType> {
-    return this.http.post<AssignmentType>("http://localhost:5286/api/AssignmentsTypes/NewAssignmentTypeAsync", assignmentType);
+    return this.http.post<AssignmentType>(environment.baseUrlApi + "api/AssignmentsTypes/NewAssignmentTypeAsync", assignmentType);
   }
 
   public getAssignmentTypesById(typeId : number) : Observable<AssignmentType> {
-    return this.http.get<AssignmentType>(`http://localhost:5286/api/AssignmentsTypes/GetAssignmentTypeByIdAsync/${typeId}`);
+    return this.http.get<AssignmentType>(environment.baseUrlApi + `api/AssignmentsTypes/GetAssignmentTypeByIdAsync/${typeId}`);
   }
 }
--- src/app/services/assignment.service.ts
+++ src/app/services/assignment.service.ts
@@ -3,6 +3,7 @@ import { Assignment } from '../models/assignment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AssignmentModelView } from '../models/assignment-model-view';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,26 +13,26 @@ export class AssignmentService {
   constructor(private http: HttpClient) { }
 
   public countAssignments(includeArchive : boolean): Observable<number> {
-    return this.http.get<number>(`http://localhost:5286/api/Assignments/CountAssignmentsAsync/${includeArchive}`);
+    return this.http.get<number>(environment.baseUrlApi + `api/Assignments/CountAssignmentsAsync/${includeArchive}`);
   }
 
   public addAssignment(assignment: Assignment): Observable<Assignment> {
-    return this.http.post<AssignmentModelView>("http://localhost:5286/api/Assignments/NewAssignmentAsync", assignment);
+    return this.http.post<AssignmentModelView>(environment.baseUrlApi + "api/Assignments/NewAssignmentAsync", assignment);
   }
 
   public getAssignments(withArchive: boolean): Observable<AssignmentModelView[]> {
-    return this.http.get<AssignmentModelView[]>(`http://localhost:5286/api/Assignments/GetAssignmentsAsync/${withArchive}`);
+    return this.http.get<AssignmentModelView[]>(environment.baseUrlApi + `api/Assignments/GetAssignmentsAsync/${withArchive}`);
   }  
 
   public assignmentSetEnded(assignment: AssignmentModelView) : Observable<Assignment> {
-    return this.http.post<AssignmentModelView>("http://localhost:5286/api/Assignments/AssignmentSetEndedAsync", assignment);
+    return this.http.post<AssignmentModelView>(environment.baseUrlApi + "api/Assignments/AssignmentSetEndedAsync", assignment);
   }
 
   public deleteAssignmentById(id : number) : Observable<Assignment> {
-    return this.http.delete<AssignmentModelView>(`http://localhost:5286/api/Assignments/DeleteAssignmentAsync/${id}`);
+    return this.http.delete<AssignmentModelView>(environment.baseUrlApi + `api/Assignments/DeleteAssignmentAsync/${id}`);
   }
 
   public archiveAssignment(assignment: AssignmentModelView) : Observable<Assignment> {
-    return this.http.post<AssignmentModelView>(`http://localhost:5286/api/Assignments/ArchiveAssignmentAsync/`, assignment);
+    return this.http.post<AssignmentModelView>(environment.baseUrlApi + `api/Assignments/ArchiveAssignmentAsync/`, assignment);
   }
 }
